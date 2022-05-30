359998f246b7f589928511124c044459aac85c1e
Jose David <josedbolivarmafrontend@gmail.com>
2022-05-30 16:54:11 -0500

    index.py

---
+++ index.py
@@ -0,0 +1,7 @@
+name = 'José'
+apellido = 'Bolívar'
+resultado = name + ' ' + apellido
+
+print("Hola mi nombre es " + resultado)
+print('Estoy empezando el curso de python')
+print('Espero aprender mucho')
