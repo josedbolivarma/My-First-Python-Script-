5e96aaf04649fea8ee559c1df0fb38f85763aa19
Jose David <josedbolivarmafrontend@gmail.com>
2022-06-12 11:20:59 -0500

    Class Exercise

--- Class.py
+++ Class.py
@@ -35,3 +35,23 @@ class Coche(Vehiculo):
 c = Coche('Rojo', 4, 4, 240, 'No')
 
 print(c.color, c.ruedas, c.puertas, c.velocidad, c.cilindrada)
+
+
+# EJERCICIO 2
+
+# En este segundo ejercicio, tendréis que crear un programa que tenga una clase llamada Alumno que tenga como atributos su nombre y su nota. Deberéis de definir los métodos para inicializar sus atributos, imprimirlos y mostrar un mensaje con el resultado de la nota y si ha aprobado o no.
+
+class Alumno:
+    nombre = ''
+    nota = 0
+    def __init__(self):
+        pass
+    def cambiarNota(self, point):
+        self.nota = point
+
+    def mostrarNota(self):
+        return self.nota
+
+a = Alumno()
+a.cambiarNota(20)
+print(a.mostrarNota())
